7cb033041717b390b10b43787092d76e80bef637
Kelly Le <kellyl17@uci.edu>
2025-05-23 22:55:48 -0700

    Updated community reports added

--- sharkspotter/backend/operations.js
+++ sharkspotter/backend/operations.js
@@ -17,8 +17,8 @@ async function insert(db, username, email, password) {
         if (!db) {
             console.error('DB Not connected yet')
         }
-        const day_input = db.collection("login");
-        await day_input.insertOne({
+        const login = db.collection("login");
+        await login.insertOne({
             username: username,
             email: email,
             password: password,
@@ -39,16 +39,36 @@ async function update(db, email, new_report) {
         if (!db) {
             console.error('DB Not connected yet')
         }
-        const day_input = db.collection("login");
-        await day_input.updateOne(
+        const reports = db.collection("login");
+        await reports.updateOne(
             {email: email,}, // find
             {$push: {report: new_report} } // update the value in that set
         );
+        const community_reports = db.collection("CommunityReports");
+        await insertCommunityReport(db, new_report);
         console.log('Updated');
     }
     catch (error) {
         console.error('Update error', error);
     }
+}
+
+
+async function insertCommunityReport(db, report) {
+    try {
+        if (!db) {
+            console.error('DB Not connected yet')
+        }
+        const community_reports = db.collection("CommunityReports");
+        await community_reports.insertOne({
+            report
+        });
+        console.log('Inserted Community Report');
+    }
+    catch (error) {
+        console.error('Insert error', error);
+        process.exit();
+   }
 }
 
 async function retrieveList(db, collection_name) {
@@ -73,7 +93,7 @@ async function execute() {
     //await insertNight(db, 'today lol', '11:00', '12:00');
     await retrieveList(db, 'login');
     await update(db, 'email', "REPORT ADD LOL")
-    await retrieveList(db, 'login');
+    await retrieveList(db, 'CommunityReports');
 }
 
 
