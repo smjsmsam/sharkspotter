fe1639dfe41e68ea01a53ae8ec51b36faf99465b
smjsmsam <smjsmsam@gmail.com>
2025-05-24 14:21:39 -0700

    empty map marker component

---
+++ shark-spotter/src/app/components/map-marker/map-marker.component.ts
@@ -0,0 +1,14 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-map-marker',
+  templateUrl: './map-marker.component.html',
+  styleUrls: ['./map-marker.component.scss'],
+})
+export class MapMarkerComponent  implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {}
+
+}
