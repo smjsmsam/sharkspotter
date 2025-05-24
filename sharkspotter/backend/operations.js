a5c4054aad5336b4fe197874995878e78eda54a2
Kelly Le <kellyl17@uci.edu>
2025-05-23 22:10:32 -0700

    Working MongoDB insert

--- sharkspotter/backend/operations.js
+++ sharkspotter/backend/operations.js
@@ -10,8 +10,56 @@ async function connect() {
     }
 }
 
-//execute();
+
+
+async function insert(db, date_input, scale_input, datetime_input) {
+    try {
+        if (!db) {
+            console.error('DB Not connected yet')
+        }
+        const day_input = db.collection("login");
+        await day_input.insertOne({
+            date: date_input,
+            scale: scale_input, 
+            datetime: datetime_input,
+        });
+        console.log('Inserted');
+    }
+    catch (error) {
+        console.error('Insert error', error);
+        process.exit();
+   }
+}
+
+
+async function retrieveList(db, collection_name) {
+    try {
+        if (!db) {
+            console.error('DB Not connected yet');
+        }
+        const collection = db.collection(collection_name);
+        const documents = await collection.find({}).toArray();
+        console.log('Documents:', documents);
+        return documents;
+    } catch (error) {
+        console.error('Fetch error', error);
+    }
+}
+
+
+async function execute() {
+    const db = await connect();
+    //await insertTime(db);
+    await insert(db, 'today lol', 100, 'timelol');
+    //await insertNight(db, 'today lol', '11:00', '12:00');
+    await retrieveList(db, 'login');
+}
+
+
+execute();
 //updateMany, deleteone, replaceOne
 module.exports = {
-    connect
+    connect,
+    insert,
+    retrieveList
 }
