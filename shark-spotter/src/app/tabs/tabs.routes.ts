91d13360608992e840c60a59e94a1a743e103d59
smjsmsam <smjsmsam@gmail.com>
2025-05-23 23:56:40 -0700

    proper navigation

--- shark-spotter/src/app/tabs/tabs.routes.ts
+++ shark-spotter/src/app/tabs/tabs.routes.ts
@@ -7,30 +7,30 @@ export const routes: Routes = [
     component: TabsPage,
     children: [
       {
-        path: 'tab1',
+        path: 'forum',
         loadComponent: () =>
-          import('../tab1/tab1.page').then((m) => m.Tab1Page),
+          import('../pages/forum/forum.page').then((m) => m.ForumPage),
       },
       {
-        path: 'tab2',
+        path: 'home',
         loadComponent: () =>
-          import('../tab2/tab2.page').then((m) => m.Tab2Page),
+          import('../pages/home/home.page').then((m) => m.HomePage),
       },
       {
-        path: 'tab3',
+        path: 'account',
         loadComponent: () =>
-          import('../tab3/tab3.page').then((m) => m.Tab3Page),
+          import('../pages/account/account.page').then((m) => m.AccountPage),
       },
       {
         path: '',
-        redirectTo: '/tabs/tab1',
+        redirectTo: '/tabs/home',
         pathMatch: 'full',
       },
     ],
   },
   {
     path: '',
-    redirectTo: '/tabs/tab1',
+    redirectTo: '/tabs/home',
     pathMatch: 'full',
   },
 ];
