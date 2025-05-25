57214f4fbdc9646d59c6025eabdca930f3129163
smjsmsam <smjsmsam@gmail.com>
2025-05-24 18:39:11 -0700

    report log

--- shark-spotter/src/app/components/report-log/report-log.component.ts
+++ shark-spotter/src/app/components/report-log/report-log.component.ts
@@ -1,14 +1,64 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { IonTitle } from '@ionic/angular/standalone';
+import { SummaryReportCardComponent } from '../summary-report-card/summary-report-card.component';
+
+interface report {
+  title:string;
+  text:string;
+  location:string;
+  time:Date;
+  longitude:number;
+  latitude:number;
+  user:string;
+}
 
 @Component({
   selector: 'app-report-log',
   templateUrl: './report-log.component.html',
   styleUrls: ['./report-log.component.scss'],
+  imports: [
+    IonTitle,
+    CommonModule,
+    SummaryReportCardComponent,
+  ]
 })
 export class ReportLogComponent  implements OnInit {
+  @Input() type!:string;
+  reports:report[] = [];
 
   constructor() { }
 
-  ngOnInit() {}
+  ngOnInit() {
+    //delete this
+    this.reports = [
+      {
+        title: "hi",
+        text: "hi",
+        location: "Irvine, CA",
+        time: new Date(),
+        latitude: 1234,
+        longitude: 1234,
+        user: ""
+      },
+      {
+        title: "hi",
+        text: "hi",
+        location: "Irvine, CA",
+        time: new Date(),
+        latitude: 1234,
+        longitude: 1234,
+        user: ""
+      }
+    ];
+
+
+    if(this.type == "personal") {
+      //TODO: retrieve user's reports
+    }
+    else if(this.type == "community") {
+      //TODO: retrieve reports in user's city
+    }
+  }
 
 }
--- shark-spotter/src/app/components/summary-report-card/summary-report-card.component.ts
+++ shark-spotter/src/app/components/summary-report-card/summary-report-card.component.ts
@@ -1,13 +1,34 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
+import {
+  IonCard,
+  IonCardTitle,
+  IonText,
+ } from '@ionic/angular/standalone';
 
 @Component({
   selector: 'app-summary-report-card',
   templateUrl: './summary-report-card.component.html',
   styleUrls: ['./summary-report-card.component.scss'],
+  imports: [
+    IonCard,
+    IonCardTitle,
+    IonText,
+  ]
 })
 export class SummaryReportCardComponent  implements OnInit {
+  @Input() title!:string;
+  @Input() text!:string;
+  @Input() location!:string;
+  @Input() time!:Date;
 
-  constructor() { }
+  constructor() { 
+    if(!this.title) {
+      this.title = "Report"
+    }
+    if(!this.text) {
+      this.text = "No description";
+    }
+  }
 
   ngOnInit() {}
 
